ec09b11bc3fbe0946660271370196c851657215e
MFivez <fivezmauritciopro@gmail.com>
2024-04-06 13:21:42 +0200

    Refactoring de la classe UI :   1 : Séparation de la classe unique UI en   UI (main), UI_CharacterState, UI_DialogueState, UI_PauseState, UI_PlayState, UI_TitleState afin de rendre le code plus modulaire

--- src/main/java/ui/I_UI.java
+++ src/main/java/ui/I_UI.java
@@ -1,14 +1,11 @@
 package ui;
 
-import enums.E_GameState;
-
 import java.awt.*;
 
 public interface I_UI {
     /** ---- draw() ----<p>
      * Cette méthode implémente toutes les UI, elle se charge de prendre en compte le dessin de la catégorie UI dans
      * laquelle est implémentée cette interface
-     *
      *
      * @param g2 Contexte graphique provenant du {@link main.GamePanel}
      */
